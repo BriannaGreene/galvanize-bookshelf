68347e81ac34af9818d774d4142dcdf4f4d13fe3
Brianna Greene <brianna.greene.designs@gmail.com>
2017-09-26 15:03:56 -0600

    part 2 almost finished

--- routes/books.js
+++ routes/books.js
@@ -11,7 +11,7 @@ const knex = require('../knex')
 // List of all books
 router.get('/books', function(req, res, next) {
   knex('books')
-  .select('id', 'title', 'author', 'genre', 'description', 'cover_url', 'created_at', 'updated_at')
+  .select('id', 'title', 'author', 'genre', 'description', 'cover_url as coverUrl', 'created_at as createdAt', 'updated_at as updatedAt')
   .orderBy('title')
   .then((items) => {
     res.setHeader('Content-Type', 'application/json')
@@ -25,8 +25,7 @@ router.get('/books/:id', function(req, res, next) {
   const id = req.params.id
 
   knex('books')
-    .select('id', 'title', 'author', 'genre', 'description', 'cover_url', 'created_at', 'updated_at')
-    .orderBy('id')
+    .select('id', 'title', 'author', 'genre', 'description', 'cover_url as coverUrl', 'created_at as createdAt', 'updated_at as updatedAt')
     .where('id', id)
     .then((items) => {
       if (items.length < 1) {
@@ -43,8 +42,16 @@ router.post('/books', function(req, res, next) {
   const { item } = req.body
 
   knex('books')
-    .insert({ item: item }, 'id')
-    .then((id) => {
+    .insert({
+      id: 9,
+      title: req.body.title,
+      author: req.body.author,
+      genre: req.body.genre,
+      description: req.body.description,
+      cover_url: req.body.coverUrl
+    })
+    .then(() => {
+      res.setHeader('Content-Type', 'application/json')
       res.sendStatus(200)
     })
     .catch((err) => next(err))
@@ -56,12 +63,13 @@ router.patch('/books/:id', function(req, res, next) {
   const id = req.params.id
 
   knex('books')
-    .update({ item: item })
+    .update({title: req.body.title, author: req.body.author, genre: req.body.genre, description: req.body.description, cover_url: req.body.coverUrl })
     .where('id', id)
     .then((rowsAffected) => {
       if (rowsAffected !== 1) {
         return res.sendStatus(404)
       }
+      res.setHeader('Content-Type', 'application/json')
       res.sendStatus(200)
     })
     .catch((err) => next(err))
@@ -78,6 +86,7 @@ router.delete('/books/:id', function(req, res, next) {
       if (rowsAffected !== 1) {
         return res.sendStatus(404)
       }
+      res.setHeader('Content-Type', 'application/json')
       res.sendStatus(200)
     })
     .catch((err) => next(err))
